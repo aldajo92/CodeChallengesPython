d7aaccd75f8494e4d83ed7ea77efd886ee82ac17
Alejandro Gómez <aldajo92@aldajo-mac.local>
2019-05-27 12:47:31 -0500

    added repo and relational_operator exercise

---
+++ src/01.input_output/relational_operator/main.py
@@ -0,0 +1,21 @@
+def return_operator(value1, value2):
+    if value1 > value2:
+        return ">"
+    elif value1 < value2:
+        return "<"
+    else:
+        return "="
+
+
+if __name__ == "__main__":
+
+    entries_size = input("Enter the number of entries\n")
+
+    list_values = []
+
+    for _ in range(int(entries_size)):
+        values = input()
+        list_values.append([int(x) for x in values.split(" ")])
+
+    for values in list_values:
+        print(return_operator(values[0], values[1]))
