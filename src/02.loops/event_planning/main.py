6dbc99431ea1ee0c2f5f16c1f940be8907006b25
Alejandro Gomez <alejodiscovery20@gmail.com>
2019-06-29 16:18:52 -0500

    added event planning part of solution

---
+++ src/02.loops/event_planning/main.py
@@ -0,0 +1,65 @@
+read_input = input()
+
+while read_input != "-1":
+    n, b, h, w = [int(x) for x in read_input.split(" ")]
+
+    cheap_cost = 0
+    weeks = 0
+    for i in range(h):
+        price = int(input())
+        arr_beds = [int(x) for x in input().split(" ")]
+
+        total_event = 0
+        w_to_take = 0
+        total_weekend = price * n
+
+        for bed in arr_beds:
+            if n <= bed and (total_event + total_weekend) <= b:
+                w_to_take += 1
+                total_event += total_weekend
+
+        # if w_to_take > weeks:
+        #     weeks = w_to_take
+        #     cheap_cost = total_weekend
+
+        if (total_weekend < cheap_cost and w_to_take > 0) or cheap_cost == 0:
+            weeks = w_to_take
+            cheap_cost = total_weekend
+
+        # print("weekend and results : {} ; {}".format(total_weekend, total_event))
+
+    if weeks == 0:
+        print("stay home")
+    else:
+        print(cheap_cost)
+
+    read_input = input()
+
+'''
+n, b, h, w = [int(x) for x in input().split(" ")]
+
+cheap_cost = 0
+weeks = 0
+for i in range(h):
+    price = int(input())
+    arr_beds = [int(x) for x in input().split(" ")]
+
+    total_event = 0
+    w_to_take = 0
+    total_weekend = price * n
+
+    for bed in arr_beds:
+        if n <= bed and (total_event + total_weekend) <= b:
+            w_to_take += 1
+            total_event += total_weekend
+
+    if (total_weekend < cheap_cost and w_to_take > 0) or cheap_cost == 0:
+        weeks = w_to_take
+        cheap_cost = total_weekend
+
+if weeks == 0:
+    print("stay home")
+else:
+    print(cheap_cost)
+
+'''
