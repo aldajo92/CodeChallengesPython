7133ed6a9e5e1d4c5cf48daa73cd6035822dce71
Alejandro Gomez <alejodiscovery20@gmail.com>
2019-06-30 00:18:56 -0500

    solved event planning with a nasty solution

--- src/02.loops/event_planning/main.py
+++ src/02.loops/event_planning/main.py
@@ -1,39 +1,39 @@
 read_input = input()
 
-while read_input != "-1":
-    n, b, h, w = [int(x) for x in read_input.split(" ")]
+try:
 
-    cheap_cost = 0
-    weeks = 0
-    for i in range(h):
-        price = int(input())
-        arr_beds = [int(x) for x in input().split(" ")]
+    while read_input is not None:
+        n, b, h, w = [int(x) for x in read_input.split(" ")]
 
-        total_event = 0
-        w_to_take = 0
-        total_weekend = price * n
+        cheap_cost = 0
+        weeks = 0
+        for i in range(h):
+            price = int(input())
+            arr_beds = [int(x) for x in input().split(" ")]
 
-        for bed in arr_beds:
-            if n <= bed and (total_event + total_weekend) <= b:
-                w_to_take += 1
-                total_event += total_weekend
+            total_event = 0
+            w_to_take = 0
+            total_weekend = price * n
 
-        # if w_to_take > weeks:
-        #     weeks = w_to_take
-        #     cheap_cost = total_weekend
+            for bed in arr_beds:
+                if n <= bed and (total_event + total_weekend) <= b:
+                    w_to_take += 1
+                    total_event += total_weekend
 
-        if (total_weekend < cheap_cost and w_to_take > 0) or cheap_cost == 0:
-            weeks = w_to_take
-            cheap_cost = total_weekend
+            if (total_weekend < cheap_cost and w_to_take > 0) or cheap_cost == 0:
+                weeks = w_to_take
+                cheap_cost = total_weekend
 
-        # print("weekend and results : {} ; {}".format(total_weekend, total_event))
+        if weeks == 0:
+            print("stay home")
+        else:
+            print(cheap_cost)
 
-    if weeks == 0:
-        print("stay home")
-    else:
-        print(cheap_cost)
+        read_input = input()
 
-    read_input = input()
+except EOFError:
+    pass
+
 
 '''
 n, b, h, w = [int(x) for x in input().split(" ")]
