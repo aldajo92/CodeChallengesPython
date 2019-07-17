22051cde92d1c8461a409371c0082fbd859ba1a2
Alejandro Gomez <alejodiscovery20@gmail.com>
2019-07-16 22:53:16 -0500

    added sorting files

---
+++ basic_algorithms/sorting/02_merge_sort.py
@@ -0,0 +1,45 @@
+def mergesort(items):
+    # Base case, a list of 0 or 1 items is already sorted
+    if len(items) <= 1:
+        return items
+
+    # Otherwise, find the midpoint and split the list
+    midpoint = len(items) // 2
+    # TODO
+    left = items[:midpoint]
+    right = items[midpoint:]
+
+    # Call mergesort recursively with the left and right half
+    left = mergesort(left)
+    right = mergesort(right)
+
+    # Merge our two halves and return
+    return merge(left, right)
+
+
+# Given two ordered lists, merge them together in order,
+# returning the merged list.
+# TODO
+def merge(left, right):
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+
+    merged += left[left_index:]
+    merged += right[right_index:]
+
+    return merged
+
+
+wakeup_times = [16, 49, 3, 12, 56, 49, 55, 22, 13, 46, 19, 55, 46, 13, 25, 56, 9, 48, 45]
+sorted_items = mergesort(wakeup_times)
+print(wakeup_times)
+print(sorted_items)
